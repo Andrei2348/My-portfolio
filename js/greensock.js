1c5e057ef7f55825599b9ddfb3fb5a7210e11d4e
Andrei2348 <makarevichandco@gmail.com>
2023-09-01 16:17:40 +0300

    anima 010923

--- js/greensock.js
+++ js/greensock.js
@@ -1,3 +1,7 @@
+// gsap.from()
+
+
 let tl = gsap.timeline();
 tl.from('.hero', {opacity: 0, duration: 1})
-tl.from('.navbar', {opacity: 0, duration: 2});
+  .from('.navbar', {opacity: 0, duration: 2})
+  .from('.hero__button-wrapper', {opacity: 0, y: 70, duration: 0.8}, "-=0.5")
